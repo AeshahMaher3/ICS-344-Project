5db99d494533bea2b621ba666aa46c67f4b37b28
AeshahMaher3 <144242273+AeshahMaher3@users.noreply.github.com>
2025-11-29 13:59:46 +0300

    Update tamper.py

--- Step 1: Setup Environment/tamper.py
+++ Step 1: Setup Environment/tamper.py
@@ -1,11 +1,11 @@
-def tamper_ciphertext(ciphertext: bytes): # this function is used to test tampering
+def tamper_ciphertext(ciphertext): # this function is used to test tampering
 
-    if not ciphertext: # check if ciphertext is empty
+    if not ciphertext: # if there is no ciphertext, nothing to modify
         return ciphertext, "No tampering applied"
     
 
-    modified_byte = ciphertext[0] ^ 0x01  # take the first byte, flip its first bit using XOR, then add the rest of the bytes back
-    modified_ciphertext = bytes([modified_byte]) + ciphertext[1:]  
+    modified_byte = ciphertext[0] ^ 0x01  # flip one bit in the first byte using XOR
+    modified_ciphertext = bytes([modified_byte]) + ciphertext[1:]  # then add the rest of the bytes back
  
     return modified_ciphertext, "Tampering applied"
  
